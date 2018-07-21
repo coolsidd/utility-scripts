0065af270d3f8edaae58e8b432f322b40de465f7
coolsidd <sidharth_0903@hotmail.com>
2018-07-21 20:37:28 +0530

    Added an eta feature so that estimated time remaining is printed Optimized so that printing progressbar does not overload the buffer

--- bits_timetabler/progressbar.py
+++ bits_timetabler/progressbar.py
@@ -1,30 +1,34 @@
+import datetime
+import time
 import os
 import sys
 
 
 class ProgressBar:
-    storedLog = []
     # this the the no. of chars that are always displayed on the screen
-    # default is 6 for "[{}{}]({__})"
-    # 					^    ^^ ^^ ^
+    # default is 6 for "[{}{}]({__})(:.7)"
+    # 			^    ^^ ^^ ^
 
     @staticmethod
     def getTermSizeWidth():
         return int(os.popen("stty size", "r").read().split()[1])
 
     def __init__(self, size=None, filler='-', empty=' '):
-        self.adjFactor = 6
+        sys.stdout.write("\b")
+        self.adjFactor = 15
+        self.filled = 0
         self.filler = filler
         self.empty = empty
         if size is None:
-            self.size = self.getTermSizeWidth()-5
+            self.size = self.getTermSizeWidth()-3
         else:
             self.size = size
-        sys.stdout.write("[{}](__)".format(
+        sys.stdout.write("[{}](PP)(timeETA)".format(
             self.empty * (self.size-self.adjFactor)))
-        # sys.stdout.flush()
-        # sys.stdout.write("\b" * (self.size-1))
-        # self.size -= 7
+        self.storedLog = []
+        sys.stdout.write("\b" * (self.size-1))
+        sys.stdout.flush()
+        self.then = time.time()
 
     def __enter__(self):
         return self
@@ -32,37 +36,42 @@ class ProgressBar:
     def log(self, k):
         self.storedLog.append(k)
 
-    def percent(self, percent):
-
-        sys.stdout.write("\b"*self.size)
+    def fraction(self, percent):
         if percent >= 1:
-            sys.stdout.write("[{}]({})".format(
-                self.filler*(self.size-self.adjFactor), "99"))
+            time_elapsed = time.time()-self.then
+            sys.stdout.write("\b"*self.size)
+            sys.stdout.write("[{}]({:2})({:.7s})".format(
+                self.filler*(self.size-self.adjFactor), "99", str(datetime.timedelta(seconds=time_elapsed))))
         else:
-            sys.stdout.write("[{}{}]({})".
-                             format(self.filler*int(percent *
-                                                    (self.size -
-                                                     self.adjFactor)),
-                                    self.empty*int((1-percent) *
-                                                   (self.size-self.adjFactor)),
-                                    str(percent*100)[:2]))
+            printed_this_much = int(
+                percent*(self.size-self.adjFactor) - self.filled)
+            sys.stdout.write(self.filler*printed_this_much)
+            self.filled += printed_this_much
+            eta = 0
+            if percent == 0:
+                self.then = time.time()
+            else:
+                eta = (time.time() - self.then)*(1/percent-1)
 
-        # self.diff = int(percent*self.size - self.filled)
-        # sys.stdout.write(self.empty*(self.size-self.filled+1))
-        # sys.stdout.write(self.
-        # if self.diff:
-        #     sys.stdout.write(self.filler*self.diff)
-        #     self.filled += self.diff
-        #     sys.stdout.flush()
+            blanks = self.size-self.filled-self.adjFactor
+            sys.stdout.write(self.empty*blanks)
+            sys.stdout.write("]({:2d})".format((int(percent*100) % 100)))
+            try:
+                sys.stdout.write("({:.7s})".format(
+                    str(datetime.timedelta(seconds=eta))))
+            except OverflowError:
+                sys.stdout.write("(forever)")
+            sys.stdout.write("\b"*(self.adjFactor-1))
+            sys.stdout.write("\b"*blanks)
         sys.stdout.flush()
 
     def __exit__(self, *a):
-        self.percent(1)
+        self.fraction(1)
         sys.stdout.flush()
         print(*self.storedLog)
 
 
-class iter(ProgressBar):
+class iterable(ProgressBar):
     def __init__(self, start, end=None, step=1, size=None,
                  filler='-', empty=' '):
         super().__init__(size, filler, empty)
@@ -82,6 +91,29 @@ class iter(ProgressBar):
             self.__exit__()
             raise StopIteration
         else:
-            self.percent(self.at/self.end)
+            self.fraction(self.at/self.end)
             self.at += self.step
             return self.at - self.step
+
+
+if __name__ == "__main__":
+    print("Testing")
+    iterations = 10000000
+    for i in iterable(iterations):
+        pass
+
+    print("Testing 2")
+    my_bar = ProgressBar()
+    for k in range(iterations):
+        my_bar.fraction(k/iterations)
+    my_bar.__exit__()
+
+    then = time.time()
+    count = 0
+
+    def do_something():
+        return count+1
+    for i in range(iterations):
+        do_something()
+    print("{:.7} iterations = {} without any bar printing".format(
+        str(datetime.timedelta(seconds=(time.time()-then))), count))
